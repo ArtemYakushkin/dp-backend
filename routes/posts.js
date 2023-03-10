ff26ee3386bc607448de14fc4a515487edfd91f7
ArtemYakushkin <zimberger29101985@gmail.com>
2023-03-10 13:41:50 +0200

    add post routers

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import { createPost, deletePost, getPost, likePost, updatePost, timelinePost } from '../controllers/postControllers.js';
 
 const router = express.Router();
+
+// create post
+router.post('/', createPost);
+// update post
+router.put('/:id', updatePost);
+// delete post
+router.delete('/:id', deletePost);
+// like/dislike a post
+router.put('/:id/like', likePost);
+// get post
+router.get('/:id', getPost);
+// get timeline post
+router.get('/timeline/all', timelinePost);
 
 
 export default router;
